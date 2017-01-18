cddc1a56f628fb4d65cdcaf00935c927625e0d33
Juliane Blier <blier.juliane@gmail.com>
2017-01-18 09:25:57 +0100

    Ajout des getters et setters

--- Utilisateur.php
+++ Utilisateur.php
@@ -11,6 +11,28 @@
 			$this->_createdAt = Date("d-m-Y H:i:s");
 		}
 
-		
+		public function getEmail(){
+			return $this->_email;
+		}
+
+		public function setEmail($newEmail){
+			$this->_email = $newEmail;
+		}
+
+		public function getId(){
+			return $this->_id;
+		}
+
+		public function setId($newId){
+			$this->_id = $newId;
+		}
+
+		public function getCreatedAt(){
+			return $this->_createdAt;
+		}
+
+		public function setCreatedAt($newDate){
+			$this->_createdAt = $newDate;
+		}
 		
 	}
