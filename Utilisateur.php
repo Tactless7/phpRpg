016aa3d6c2803f05ed2fb8f24b91795bf36007ef
Juliane Blier <blier.juliane@gmail.com>
2017-01-18 09:25:35 +0100

    Création du constructeur

--- Utilisateur.php
+++ Utilisateur.php
@@ -4,6 +4,13 @@
 		private $_email;
 		private $_id;
 		private $_createdAt;
+		
+		public function __construct($email, $id){
+			$this->_email = $email;
+			$this->_id = $id;
+			$this->_createdAt = Date("d-m-Y H:i:s");
+		}
 
+		
 		
 	}
