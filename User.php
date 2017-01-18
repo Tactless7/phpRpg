ac6afa62edc2ad16e6e782f5af57396d5261c909
Juliane Blier <blier.juliane@gmail.com>
2017-01-18 09:28:16 +0100

    Modification du nom du fichier Utilisateur en User

---
+++ User.php
@@ -0,0 +1,38 @@
+<?php 
+	
+	class Utilisateur{
+		private $_email;
+		private $_id;
+		private $_createdAt;
+		
+		public function __construct($email, $id){
+			$this->_email = $email;
+			$this->_id = $id;
+			$this->_createdAt = Date("d-m-Y H:i:s");
+		}
+
+		public function getEmail(){
+			return $this->_email;
+		}
+
+		public function setEmail($newEmail){
+			$this->_email = $newEmail;
+		}
+
+		public function getId(){
+			return $this->_id;
+		}
+
+		public function setId($newId){
+			$this->_id = $newId;
+		}
+
+		public function getCreatedAt(){
+			return $this->_createdAt;
+		}
+
+		public function setCreatedAt($newDate){
+			$this->_createdAt = $newDate;
+		}
+		
+	}
