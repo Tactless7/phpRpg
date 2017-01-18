b19bd7289e59f734f84e996f143619444f6a556b
Juliane Blier <blier.juliane@gmail.com>
2017-01-18 17:05:52 +0100

    Ajout d'un formulaire d'achat

---
+++ shopping.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Let's Shop !</title>
+</head>
+<body>
+
+	<h1>Venez Acheter chez nous, c'est presque gratuit !</h1>
+	<form action="">
+		<select name="client" id="client">
+			<option value="janeDoe">Jane Doe</option>
+			<option value="johnSmith">John Smith</option>
+		</select>
+		<select name="product1" id="product1">
+			<option value="banane">Banane</option>
+			<option value="poireau">Poireau</option>
+			<option value="veste">Veste</option>
+			<option value="sacAMain">Sac à Main</option>
+			<option value="jeanBrut">Jean Brut</option>
+		</select>
+		<select name="product1" id="product1">
+			<option value="banane">Banane</option>
+			<option value="poireau">Poireau</option>
+			<option value="veste">Veste</option>
+			<option value="sacAMain">Sac à Main</option>
+			<option value="jeanBrut">Jean Brut</option>
+		</select>
+		<select name="product1" id="product1">
+			<option value="banane">Banane</option>
+			<option value="poireau">Poireau</option>
+			<option value="veste">Veste</option>
+			<option value="sacAMain">Sac à Main</option>
+			<option value="jeanBrut">Jean Brut</option>
+		</select>
+		<button type="submit">Buy</button>
+	</form>
+	
+</body>
+</html>
