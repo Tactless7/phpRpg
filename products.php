f73c97e0e5fac8bece4491d70de880cfbf6185b3
Juliane Blier <blier.juliane@gmail.com>
2017-01-18 16:45:13 +0100

    utilisation de require et  non include

--- products.php
+++ products.php
@@ -1,3 +1,3 @@
 <?php 
 
-include __DIR__.'/views/productTable.php' ?>
+require __DIR__.'/views/productTable.php' ?>
--- users.php
+++ users.php
@@ -1,3 +1,3 @@
 <?php 
 
-include __DIR__.'/views/usersTable.php' ?>
+require __DIR__.'/views/usersTable.php' ?>
