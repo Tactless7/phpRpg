d84529c5da92a6db9681d8227db200e3d9dad276
Juliane Blier <blier.juliane@gmail.com>
2017-01-18 09:48:42 +0100

    Création du tableau HTML qui contiendra les données utilisateurs

---
+++ usersTable.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Users Table</title>
+</head>
+<body>
+	
+	<table>
+		<tr>
+			<th>Id</th>
+			<th>Email</th>
+			<th>Created At</th>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	</table>
+
+	
+</body>
+</html>
