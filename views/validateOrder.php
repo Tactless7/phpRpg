d6e3deda88424a94c4abf451d24de6ed17688af7
Juliane Blier <blier.juliane@gmail.com>
2017-01-19 12:30:10 +0100

    Ajout de l'affichage du cart et du total

--- views/validateOrder.php
+++ views/validateOrder.php
@@ -3,8 +3,25 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Validate Order</title>
+	<link rel="stylesheet" href="../style.css">
 </head>
 <body>
+	<h1>Order Validation</h1>
+
+	<table>
+		<tr>
+			<th>Product Name</th>
+			<th>Unit Price</th>
+		</tr>
+			<?php foreach ($userArray[$idClient]->getCart() as $key => $value): ?>
+		<tr>
+			<td><?= $value->getName()?></td>
+			<td><?= $value->getPrice() ?></td>
+		</tr>
+			<?php endforeach ?>
+	</table>
+
+	<h3>Total Price : <?= $userArray[$idClient]->getBillAmount() ?> $</h3>
 	
 </body>
 </html>
