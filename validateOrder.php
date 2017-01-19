4b651795577dde4879f5dbabc22eae253a2f9f86
Juliane Blier <blier.juliane@gmail.com>
2017-01-19 11:19:53 +0100

    Récupération des données en POST

---
+++ validateOrder.php
@@ -0,0 +1,9 @@
+<?php 
+
+	
+	$idClient = $_POST['client'];
+	$idProduct1 = $_POST['product1'];
+	$idProduct2 = $_POST['product2'];
+	$idProduct3 = $_POST['product3'];
+
+	require_once __DIR__.'/views/validateOrder.php';
