1d6abda4002d89ad3b464660d5297ee08451c953
Juliane Blier <blier.juliane@gmail.com>
2017-01-23 10:26:05 +0100

    Ajout de la structure HTML de la page des produits périmés

---
+++ views/oldProduct.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Produit Périmé</title>
+  </head>
+  <body>
+    <h2>Produit périmé</h2>
+    Le produit commandé <?= {nom du produit} ?> n'est pas frais.
+  </body>
+</html>
